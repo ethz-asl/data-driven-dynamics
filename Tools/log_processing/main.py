768f8e4972acd197aec2a048c63900a599c5f3b6
Manumerous <manuel.galliker@gmx.ch>
2021-03-08 19:00:30 +0100

    started working on project structure

---
+++ Tools/log_processing/main.py
@@ -0,0 +1,13 @@
+
+__author__ = "Manuel Galliker"
+__maintainer__ = "Manuel Galliker"
+__license__ = "GPL"
+
+
+def main():
+    print("hello")
+    return
+
+
+if __name__ == "__main__":
+    main()
