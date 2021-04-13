Safeguard for actuator normalization and fixing stall angle conversion A safeguard ensuring that the actuator normalization never produces negative values was added. This was needed since for example vertical rotors of the standart vtol quadplane have actuator values of 900 in fixed wing mode, even though their minimal pwm is specified as 1000.
Secondly the stall angle was currently specified in degrees without converting it to radians. This conversion is needed to use it together with the angle of attack and the trigonometric functions of the math library. The default stall angle was increased to 35 degrees for now.

Tools/parametric_model/src/models/aerodynamic_models/linear_plate_aero_model.py
@@ -10,8 +10,8 @@ from scipy.spatial.transform import Rotation
 
 
 class LinearPlateAeroModel():
-    def __init__(self, stall_angle=20.0):
-        self.stall_angle = stall_angle
+    def __init__(self, stall_angle=35.0):
+        self.stall_angle = stall_angle*math.pi/180.0
 
     def compute_main_wing_feature(self, v_airspeed, angle_of_attack):
         """