2624769b1b697efc088ab00aff9546ee890b3516
Manumerous <manuel.galliker@gmx.ch>
2021-04-23 11:25:45 +0200

    Adapting rotor model to estimate inflow scaler Instead of hardcoding the an inflow scaler we now estimate the airspeed, perpendicular to the rotor axis, at which the propeller does not produce any thrust anymore.
    In short: The blades of the propeller expierence a relative airflow composed of the inflow of the airspeed of the vehicle and the rotation of the blade itself. At a certain angle of attack of the airflow relative to the blade the propeller blade will not produce any lift anymore and sebsequentially the rotor does not procude any thrust anymore. Since the ratio between incoming airspeed and blade speed due to the angular velocity is responsible for this angle of attack we can derive a model for rotor thrust depending on angular velocity and angular velocity squared. More details will follow in the model description pdf.

--- Tools/parametric_model/src/models/rotor_models/gazebo_rotor_model.py
+++ Tools/parametric_model/src/models/rotor_models/gazebo_rotor_model.py
@@ -21,29 +21,17 @@ class GazeboRotorModel():
         actuator_input: actuator input between 0 and 1
         v_airspeed: airspeed velocity in body frame, numpoy array of shape (3,1)
 
-        Model:
-        angular_vel [rad/s] = angular_vel_const*actuator_input + angular_vel_offset
-        inflow_scaler = 1 - v_airspeed_parallel_to_rotor_axis/self.max_rotor_inflow_vel
-        F_thrust = inflow_scaler * mot_const * angular_vel^2 * rotor_axis_vec
-        F_drag = - angular_vel * drag_coef * v_airspeed_perpendicular_to_rotor_axis_vec
-
-        Rotor Thrust Features:
-        F_thrust/m = (c_2 * actuator_input^2 + c_1 * actuator_input + c_0)* rotor_axis_vec
-        F_thrust/m = X_thrust @ (c_2, c_1, c_0)^T
-
-        Rotor Drag Features: 
-        F_drag/m = X_drag @ (c_4, c_3)^T = v_airspeed_perpendicular_to_rotor_axis_vec @ (u, 1) @ (c_4, c_3)^T
+        For the model explanation have a look at the PDF.
         """
 
-        # Thrust computation
+        # Thrust force computation
         v_airspeed_parallel_to_rotor_axis = np.vdot(
             self.rotor_axis, v_airspeed) * self.rotor_axis
-        vel = np.linalg.norm(v_airspeed_parallel_to_rotor_axis)
-        inflow_scaler = 1 - vel/self.max_rotor_inflow_vel
-        inflow_scaler = max(0, inflow_scaler)
-        X_thrust = inflow_scaler * self.rotor_axis @ np.array(
-            [[actuator_input**2, actuator_input, 1]])
-        # Drag computation
+        v_air_parallel = np.linalg.norm(v_airspeed_parallel_to_rotor_axis)
+
+        X_thrust = self.rotor_axis @ np.array(
+            [[actuator_input**2, (actuator_input*v_air_parallel)]])
+        # Drag force computation
         v_airspeed_perpendicular_to_rotor_axis = v_airspeed - \
             v_airspeed_parallel_to_rotor_axis
         if (np.linalg.norm(v_airspeed_perpendicular_to_rotor_axis) >= 0.1):
@@ -53,6 +41,10 @@ class GazeboRotorModel():
             X_drag = np.zeros((3, 1))
 
         X_forces = np.hstack((X_drag, X_thrust))
+
+        # Thrust leaver moment
+
+        # Rotor drag moment
 
         return X_forces
 
@@ -69,5 +61,5 @@ class GazeboRotorModel():
                 actuator_input_vec[i], v_airspeed_mat[i, :].reshape((3, 1)))
             X_actuator_forces = np.vstack((X_actuator_forces, X_curr))
         coef_list = ["rot_drag_lin", "rot_thrust_quad",
-                     "rot_thrust_lin", "rot_thrust_offset"]
+                     "rot_thrust_lin"]
         return X_actuator_forces, coef_list
