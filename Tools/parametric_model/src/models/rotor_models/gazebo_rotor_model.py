cd5304f8bebca0ab533d548eaf80ac16c4a28803
Manumerous <manuel.galliker@gmx.ch>
2021-04-25 16:01:11 +0200

    Added Rotor Moments Added the computation of the features needed to find the parameters to predict the moments generated by the propeller. The current moments include the thrust leaver moment (generated by the thrust force), the rotor drag moment (due to the rotor blades) and the rolling moment (due to sideways airflow generating different aispeeds for advancing and retracting prop blades).

--- Tools/parametric_model/src/models/rotor_models/gazebo_rotor_model.py
+++ Tools/parametric_model/src/models/rotor_models/gazebo_rotor_model.py
@@ -8,16 +8,16 @@ import math
 
 
 class GazeboRotorModel():
-    def __init__(self, rotor_axis=np.array([[0], [0], [-1]]), max_rotor_inflow_vel=25.0, rotor_direction=1):
+    def __init__(self, rotor_axis=np.array([[0], [0], [-1]]), rotor_position=np.array([[1], [1], [0]]),  max_rotor_inflow_vel=25.0, turning_direction=1):
         # no more thrust produced at this airspeed inflow velocity
         self.max_rotor_inflow_vel = max_rotor_inflow_vel
         self.rotor_axis = rotor_axis.reshape((3, 1))
-        self.rotrotor_direction = rotor_direction
+        self.turning_direction = turning_direction
 
     def compute_actuator_force_features(self, actuator_input, v_airspeed):
         """compute thrust model using a 2nd degree model of the normalized actuator outputs
 
-        Inputs: 
+        Inputs:
         actuator_input: actuator input between 0 and 1
         v_airspeed: airspeed velocity in body frame, numpoy array of shape (3,1)
 
@@ -42,24 +42,39 @@ class GazeboRotorModel():
 
         X_forces = np.hstack((X_drag, X_thrust))
 
+        X_moments = np.zeros((3, 7))
         # Thrust leaver moment
+        X_moments[0, 0] = actuator_input**2
+        X_moments[0, 1] = actuator_input*v_air_parallel
+        X_moments[1, 2] = actuator_input**2
+        X_moments[1, 3] = actuator_input*v_air_parallel
 
         # Rotor drag moment
+        X_moments[2, 4] = - self.turning_direction * actuator_input**2
+        X_moments[2, 5] = - self.turning_direction * \
+            actuator_input*v_air_parallel
 
-        return X_forces
+        # Rotor Rolling Moment
+        X_moments[:, 6] = (np.cross(np.cross(
+            v_airspeed_perpendicular_to_rotor_axis.flatten(), self.rotor_axis.flatten()), self.rotor_axis.flatten())).reshape(3)
+
+        return X_forces, X_moments
 
     def compute_actuator_feature_matrix(self, actuator_input_vec, v_airspeed_mat):
         """
-        Inputs: 
+        Inputs:
         actuator_input_vec: vector of actuator inputs (normalized between 0 and 1), numpy array of shape (n, 1)
         v_airspeed_mat: matrix of vertically stacked airspeed vectors, numpy array of shape (n, 3)
         """
-        X_actuator_forces = self.compute_actuator_force_features(
+        X_forces, X_moments = self.compute_actuator_force_features(
             actuator_input_vec[0], v_airspeed_mat[0, :].reshape((3, 1)))
         for i in range(1, actuator_input_vec.shape[0]):
-            X_curr = self.compute_actuator_force_features(
+            X_force_curr, X_moment_curr = self.compute_actuator_force_features(
                 actuator_input_vec[i], v_airspeed_mat[i, :].reshape((3, 1)))
-            X_actuator_forces = np.vstack((X_actuator_forces, X_curr))
-        coef_list = ["rot_drag_lin", "rot_thrust_quad",
-                     "rot_thrust_lin"]
-        return X_actuator_forces, coef_list
+            X_forces = np.vstack((X_forces, X_force_curr))
+            X_moments = np.vstack((X_moments, X_moment_curr))
+        coef_list_forces = ["rot_drag_lin", "rot_thrust_quad",
+                            "rot_thrust_lin"]
+        coef_list_moments = ["c_m_leaver_x_quad", "c_m_leaver_x_lin", "c_m_leaver_y_quad", "c_m_leaver_y_lin",
+                             "c_m_drag_z_quad", "c_m_drag_z_lin"]
+        return X_forces, X_moments, coef_list_forces, coef_list_moments
