Fixed sign for rotor drag features The sign for the computation of the rotor drag coefficient was was pointing in the wrong way, resulting in the corresponding rotor drag coefficient to always be negative.

Tools/parametric_model/src/models/rotor_models/rotor_model.py
@@ -34,8 +34,8 @@ class RotorModel():
         # Drag force computation
         v_airspeed_perpendicular_to_rotor_axis = v_airspeed - \
             v_airspeed_parallel_to_rotor_axis
-        if (np.linalg.norm(v_airspeed_perpendicular_to_rotor_axis) >= 0.1):
-            X_drag = v_airspeed_perpendicular_to_rotor_axis @ np.array(
+        if (np.linalg.norm(v_airspeed_perpendicular_to_rotor_axis) >= 0.05):
+            X_drag = - v_airspeed_perpendicular_to_rotor_axis @ np.array(
                 [[actuator_input]])
         else:
             X_drag = np.zeros((3, 1))