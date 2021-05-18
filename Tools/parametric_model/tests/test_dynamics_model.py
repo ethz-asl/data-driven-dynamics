fcdf908e261235871be05b9bc9f7864f730bda7c
Manumerous <manuel.galliker@gmx.ch>
2021-05-19 00:34:10 +0200

    Adding testing structure for tilting rotor model and visual dataframe selector The tilting rotor model is now tested for one of its main additions to the standard rotor model: the tilting of the axis. Also the visual dataframe selector is not tested whenever the shell script for testing is used. Here the user needs to manually close the window.

--- Tools/parametric_model/tests/test_dynamics_model.py
+++ Tools/parametric_model/tests/test_dynamics_model.py
@@ -2,19 +2,17 @@ __author__ = "Manuel Galliker"
 __maintainer__ = "Manuel Galliker"
 __license__ = "BSD 3"
 
-from _pytest.python import Class
-import pytest
-from src.models import DynamicsModel
-from src.models import ModelConfig
 from src.tools.math_tools import rmse_between_numpy_arrays
+from src.models import ModelConfig
+from src.models import DynamicsModel
+import os
 
 
-def test_transformations(config_file="dynamics_model_test_config.yaml"):
+def test_transformations():
+    config = ModelConfig("dynamics_model_test_config.yaml")
     # Setup model with reference log
-    config = ModelConfig(config_file)
     model = DynamicsModel(config_dict=config.dynamics_model_config,
                           rel_data_path="resources/quadrotor_model.ulg")
-
     # Add gravity vector to inertial accelerations
     model.data_df["az"] -= 9.81
 
@@ -24,13 +22,26 @@ def test_transformations(config_file="dynamics_model_test_config.yaml"):
         "accelerometer_m_s2[0]", "accelerometer_m_s2[1]", "accelerometer_m_s2[2]"]].to_numpy()
 
     # Check transform from inertial NED to FRD body frame:
-    accel_NED_transformed_to_FRD = model.rot_to_body_frame(accel_NED_mat)
+    accel_NED_transformed_to_FRD = model.rot_to_body_frame(
+        accel_NED_mat)
     assert (rmse_between_numpy_arrays(
         accel_FRD_mat, accel_NED_transformed_to_FRD) <= 0.1)
 
     # Check transform from FRD body frame to inertial NED frame:
-    accel_FRD_transformed_to_NED = model.rot_to_world_frame(accel_FRD_mat)
+    accel_FRD_transformed_to_NED = model.rot_to_world_frame(
+        accel_FRD_mat)
     assert (rmse_between_numpy_arrays(
         accel_NED_mat, accel_FRD_transformed_to_NED) <= 0.1)
 
     return
+
+
+# Run uas module using 'python3 -m tests/test_dynamics_model' for development and testing of the test.
+if __name__ == "__main__":
+    # set cwd to project directory when run as module
+    cwd = os.getcwd()
+    parent = os.path.join(cwd, os.pardir)
+    des_cwd = os.path.join(parent, os.pardir)
+    os.chdir(des_cwd)
+
+    test_transformations()
