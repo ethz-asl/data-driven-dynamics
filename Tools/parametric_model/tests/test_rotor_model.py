695ce4812a58ca918e1c1951d6797b0ed231a768
Manumerous <manuel.galliker@gmx.ch>
2021-05-23 17:56:53 +0200

    Adding rotor force prediciton test

--- Tools/parametric_model/tests/test_rotor_model.py
+++ Tools/parametric_model/tests/test_rotor_model.py
@@ -30,6 +30,34 @@ def test_local_airspeed_computation():
         rotor.v_airspeed_perpendicular_to_rotor_axis[2, :], np.array([0, 0, 5]))
 
 
+def test_rotor_thrust_prediction():
+    rotor_config_dict = {"description": "test rotor",
+                         "dataframe_name": "u0",
+                         "rotor_type": "RotorModel",
+                         "rotor_axis": [0, 0, -1],
+                         "turning_direction": 1,
+                         "position": [0, 0, 0]}
+
+    actuator_input_vec = np.array([0, 0.5, 1, 0, 0.5, 1])
+    v_airspeed_mat = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0], [
+                              0, 0, -5], [0, 0, -5], [0, 0, -5]])
+    thrust_coef_list = [10, -1]
+
+    correct_force_prediction = np.array([[0,       0,       0.],
+                                         [0,       0,  -3.0625],
+                                         [0,       0,   -12.25],
+                                         [0,       0,       0.],
+                                         [0,       0,       0.],
+                                         [0,       0,  -6.125]])
+
+    rotor = RotorModel(rotor_config_dict, actuator_input_vec, v_airspeed_mat)
+    X_moments, coef_list_moments = rotor.compute_actuator_force_matrix()
+    print(rotor.predict_thrust_force(
+        thrust_coef_list))
+    assert (np.array_equal(rotor.predict_thrust_force(
+        thrust_coef_list), correct_force_prediction))
+
+
 if __name__ == "__main__":
     # set cwd to project directory when run as module
     cwd = os.getcwd()
@@ -38,3 +66,4 @@ if __name__ == "__main__":
     os.chdir(des_cwd)
 
     test_local_airspeed_computation()
+    test_rotor_thrust_prediction()
--- Tools/parametric_model/tests/test_tilting_rotor.py
+++ Tools/parametric_model/tests/test_tilting_rotor.py
@@ -8,6 +8,38 @@ import numpy as np
 import math
 
 
+def test_rotor_thrust_prediction():
+    rotor_config_dict = {"description": "test rotor",
+                         "dataframe_name": "u0",
+                         "rotor_type": "TiltingRotorModel",
+                         "tilt_actuator_dataframe_name": "u_tilt",
+                         "rotor_axis": [1, 0, 0],
+                         "tilt_axis": [0, 1, 0],
+                         "max_tilt_angle_deg": 90,
+                         "turning_direction": 1,
+                         "position": [0, 0, 0]}
+
+    tilt_actuator_vec = np.array([0, 0.5, 1, 0, 0.5, 1])
+    actuator_input_vec = np.array([0.5, 0.5, 0.5, 1, 1, 1])
+    v_airspeed_mat = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0], [
+                              0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    rotor = TiltingRotorModel(rotor_config_dict, actuator_input_vec, v_airspeed_mat,
+                              tilt_actuator_vec)
+
+    thrust_coef_list = [10, -1]
+    correct_force_prediction = np.array([[3.06250000,   0,            0],
+                                         [2.16551452,   0,  -2.16551452],
+                                         [0,            0,      -3.0625],
+                                         [12.25,        0,            0],
+                                         [8.66205807,   0,  -8.66205807],
+                                         [0,            0,       -12.25]])
+    X_moments, coef_list_moments = rotor.compute_actuator_force_matrix()
+    print(np.linalg.norm(rotor.predict_thrust_force(
+        thrust_coef_list) - correct_force_prediction))
+    assert (np.linalg.norm(rotor.predict_thrust_force(
+        thrust_coef_list) - correct_force_prediction) < 10e-8)
+
+
 def test_local_airspeed_computation():
     rotor_config_dict = {"description": "test rotor",
                          "dataframe_name": "u0",
@@ -123,6 +155,7 @@ if __name__ == "__main__":
     des_cwd = os.path.join(parent, os.pardir)
     os.chdir(des_cwd)
 
+    test_rotor_thrust_prediction()
     test_local_airspeed_computation()
     test_tilting_x_axis()
     test_tilting_y_axis()
