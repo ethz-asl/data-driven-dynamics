866212afcbbe32f9c67b3e8d1ff03e6b74bcea2b
Manumerous <manuel.galliker@gmx.ch>
2021-07-15 09:31:45 +0200

    Added Moment estimation to quadrotor model
    Moment estimation was added to the quadrotor model. Currently it does not use the specified rotor position yet.

--- Tools/parametric_model/src/models/multirotor_model.py
+++ Tools/parametric_model/src/models/multirotor_model.py
@@ -24,6 +24,7 @@ Note that the forces are calculated in the NED body frame and are therefore nega
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from scipy.linalg import block_diag
 
 from sklearn.linear_model import LinearRegression
 from .dynamics_model import DynamicsModel
@@ -49,13 +50,38 @@ class MultiRotorModel(DynamicsModel):
             self.normalize_actuators()
             self.compute_airspeed_from_groundspeed(["vx", "vy", "vz"])
 
-        accel_mat = self.data_df[[
-            "accelerometer_m_s2[0]", "accelerometer_m_s2[1]", "accelerometer_m_s2[2]"]].to_numpy()
-        self.data_df[["ax_body", "ay_body", "az_body"]] = accel_mat
-        y = (accel_mat).flatten()
+            self.compute_rotor_features(self.rotor_config_dict)
 
-        # Rotor features
-        self.compute_rotor_features(self.rotor_config_dict)
+        if (self.estimate_forces and self.estimate_moments):
+            self.prepare_force_regression_matrices()
+            self.prepare_moment_regression_matrices()
+
+            self.X = block_diag(self.X_forces, self.X_moments)
+            self.y = np.hstack((self.y_forces, self.y_moments))
+
+        elif (self.estimate_forces):
+            self.prepare_force_regression_matrices()
+
+            self.X = self.X_forces
+            self.y = self.y_forces
+
+        elif (self.estimate_moments):
+            self.prepare_moment_regression_matrices()
+
+            self.X = self.X_moments
+            self.y = self.y_moments
+
+        else:
+            print("ERROR: Neither Forces nor Moments estimation activated")
+            exit(1)
+
+        return self.X, self.y
+
+    def prepare_force_regression_matrices(self):
+
+        accel_mat = self.data_df[[
+            "acc_b_x", "acc_b_y", "acc_b_z"]].to_numpy()
+        self.y_forces = (accel_mat).flatten()
 
         aoa_mat = self.data_df[["AoA"]].to_numpy()
         airspeed_mat = self.data_df[["V_air_body_x",
@@ -65,53 +91,44 @@ class MultiRotorModel(DynamicsModel):
             airspeed_mat, aoa_mat)
         self.coef_name_list.extend(
             self.rotor_forces_coef_list + aero_coef_list)
-        X = np.hstack((self.X_rotor_forces, X_aero))
+        self.X_forces = np.hstack((self.X_rotor_forces, X_aero))
         print("datapoints for self.regression: ", self.data_df.shape[0])
-        # Hacking the quadrotor model to stay consistent with other models
-        self.X = X
-        self.y = y
-        return X, y
+
+    def prepare_moment_regression_matrices(self):
+        ang_accel_mat = self.data_df[[
+            "ang_acc_b_x", "ang_acc_b_y", "ang_acc_b_z"]].to_numpy()
+        self.y_moments = (ang_accel_mat).flatten()
+        self.X_moments = self.X_rotor_moments
+        self.coef_name_list.extend(self.rotor_moments_coef_list)
 
     def plot_model_predicitons(self):
 
         y_pred = self.reg.predict(self.X)
 
-        model_plots.plot_accel_predeictions(
-            self.y, y_pred, self.data_df["timestamp"])
-        model_plots.plot_airspeed_and_AoA(
-            self.data_df[["V_air_body_x", "V_air_body_y", "V_air_body_z", "AoA"]], self.data_df["timestamp"])
-        plt.show()
+        if (self.estimate_forces and self.estimate_moments):
+            y_forces_pred = y_pred[0:self.y_forces.shape[0]]
+            y_moments_pred = y_pred[self.y_forces.shape[0]:]
+            model_plots.plot_accel_predeictions(
+                self.y_forces, y_forces_pred, self.data_df["timestamp"])
+            model_plots.plot_angular_accel_predeictions(
+                self.y_moments, y_moments_pred, self.data_df["timestamp"])
+            model_plots.plot_airspeed_and_AoA(
+                self.data_df[["V_air_body_x", "V_air_body_y", "V_air_body_z", "AoA"]], self.data_df["timestamp"])
+            plt.show()
+
+        elif (self.estimate_forces):
+            y_forces_pred = y_pred
+            model_plots.plot_accel_predeictions(
+                self.y_forces, y_forces_pred, self.data_df["timestamp"])
+            model_plots.plot_airspeed_and_AoA(
+                self.data_df[["V_air_body_x", "V_air_body_y", "V_air_body_z", "AoA"]], self.data_df["timestamp"])
+            plt.show()
+
+        elif (self.estimate_moments):
+            y_moments_pred = y_pred
+            model_plots.plot_angular_accel_predeictions(
+                self.y_moments, y_moments_pred, self.data_df["timestamp"])
+            model_plots.plot_airspeed_and_AoA(
+                self.data_df[["V_air_body_x", "V_air_body_y", "V_air_body_z", "AoA"]], self.data_df["timestamp"])
+            plt.show()
         return
-
-    def plot_motor_model_prediction(self):
-        # plot model prediction
-        u = np.linspace(0.0, 1, num=101, endpoint=True)
-        u_coll_pred = self.rotor_count*u
-        u_squared_coll_pred = self.rotor_count * u**2
-        y_pred = np.zeros(u.size)
-        coef_dict = self.result_dict["coefficients"]
-        for i in range(u.size):
-            y_pred[i] = coef_dict["vert_rot_thrust_quad"]*u_squared_coll_pred[i] + \
-                coef_dict["vert_rot_thrust_lin"]*u_coll_pred[i] + \
-                coef_dict["vert_rot_thrust_offset"]
-        plt.plot(u_coll_pred, y_pred, label='prediction')
-        # plot underlying data
-        y_data = self.data_df["az_body"].to_numpy()
-        u_coll_data, u_squared_coll_data = self.compute_collective_input_features()
-        plt.plot(u_coll_data, y_data, 'o', label='data')
-        plt.ylabel('acceleration in z direction [m/s^2]')
-        plt.xlabel('collective input (between [0, 1] per input)')
-        plt.legend()
-        plt.show()
-
-    def compute_collective_input_features(self):
-        u_collective = np.ones(self.data_df.shape[0])
-        u_squared_collective = np.ones(self.data_df.shape[0])
-        for r in range(self.data_df.shape[0]):
-            u0 = self.data_df["u0"].iloc[r]
-            u1 = self.data_df["u1"].iloc[r]
-            u2 = self.data_df["u2"].iloc[r]
-            u3 = self.data_df["u3"].iloc[r]
-            u_collective[r] = u0 + u1 + u2 + u3
-            u_squared_collective[r] = u0**2 + u1**2 + u2**2 + u3**2
-        return u_collective, u_squared_collective
