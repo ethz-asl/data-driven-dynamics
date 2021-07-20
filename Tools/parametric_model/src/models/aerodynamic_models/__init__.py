a2b5fd88f4fb7bc18d8fca76ade0aa1eb7a7821d
Manumerous <manuel.galliker@gmx.ch>
2021-07-20 23:40:40 +0200

    Adding standard plane model This commit adds the standard plane model and adapts the generation of the stadard vtol model to be based on this new model.

--- Tools/parametric_model/src/models/aerodynamic_models/__init__.py
+++ Tools/parametric_model/src/models/aerodynamic_models/__init__.py
@@ -10,3 +10,4 @@ from .aero_model_Delta import AeroModelDelta
 from .tilt_wing_section_aero_model import TiltWingSection
 from .fuselage_drag_model import FuselageDragModel
 from .elevator_model import ElevatorModel
+from .standard_wing_model import StandardWingModel
