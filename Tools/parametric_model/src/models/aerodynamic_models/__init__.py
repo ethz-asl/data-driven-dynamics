ce9c4a00060eb4450660a7f7b67b5a6175b59f25
Manumerous <manuel.galliker@gmx.ch>
2021-08-03 21:30:04 +0200

    Removing tilting wing section model component
    Since the tilting wing section is pretty specific to the tiltwing model and since it is not actively used in any other model I propose to remove it from the default building blocks of the pipeline together with the tiltwing model.

--- Tools/parametric_model/src/models/aerodynamic_models/__init__.py
+++ Tools/parametric_model/src/models/aerodynamic_models/__init__.py
@@ -7,7 +7,6 @@ __license__ = "BSD 3"
 
 from .aero_model_AAE import AeroModelAAE
 from .aero_model_Delta import AeroModelDelta
-from .tilt_wing_section_aero_model import TiltWingSection
 from .fuselage_drag_model import FuselageDragModel
 from .elevator_model import ElevatorModel
 from .standard_wing_model import StandardWingModel
