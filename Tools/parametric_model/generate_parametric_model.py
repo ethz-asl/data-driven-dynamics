7045d2b94ecc60eeace3dfe99c122c59a48dcbd9
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2021-08-03 17:07:33 +0200

    Self-describing config yamls (#134)
    * Self-describing config yamls
    
    * Fix rebase conflicts
    
    Co-authored-by: Jaeyoung-Lim <jalim@ethz.ch>

--- Tools/parametric_model/generate_parametric_model.py
+++ Tools/parametric_model/generate_parametric_model.py
@@ -5,7 +5,7 @@ __license__ = "BSD 3"
 import os
 import sys
 import inspect
-from src.models import MultiRotorModel, StandardPlaneModel, DeltaQuadPlaneModel, TiltWingModel
+import src.models as models
 from src.tools import DataHandler
 import argparse
 
@@ -23,36 +23,30 @@ def str2bool(v):
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
-def start_model_estimation(arg_list):
-    model_name = arg_list.model
-    data_selection_enabled = arg_list.data_selection
+def start_model_estimation(config, log_path, data_selection=False):
+    data_selection_enabled = data_selection
     print("Visual Data selection enabled: ", data_selection_enabled)
-    config_file = arg_list.config
 
-    data_handler = DataHandler(config_file)
-    data_handler.loadLogs(arg_list.log_path)
+    data_handler = DataHandler(config)
+    data_handler.loadLogs(log_path)
+
     if data_selection_enabled:
         data_handler.visually_select_data()
     data_handler.visualize_data()
 
     data_df = data_handler.get_dataframes()
 
-    if (model_name == "quadrotor_model"):
-        model = MultiRotorModel(config_file, model_name="quadrotor_model")
+    model_class = data_handler.config.model_class
+    try:
+        # This will call the model constructor directly from the model_class
+        # in the yaml config (self-describing)
+        # i.e if model_name is MultiRotorModel then it will call that __init__()
+        model = getattr(models, model_class)(config)
+    except AttributeError:
+        error_str = "Model '{0}' not found, is it added to models "\
+                    "directory and models/__init__.py?".format(model_class)
+        raise AttributeError(error_str)
 
-    elif (model_name == "quadplane_model"):
-        model = StandardPlaneModel(config_file, model_name="quadplane_model")
-
-    elif (model_name == "delta_quadplane_model"):
-        model = DeltaQuadPlaneModel(config_file)
-
-    elif (model_name == "standardplane_model"):
-        model = StandardPlaneModel(config_file, model_name="standardplane_model")
-
-    elif (model_name == "tiltwing_model"):
-        model = TiltWingModel(config_file)
-    else:
-        print("no valid model selected")
     model.load_dataframes(data_df)
     model.estimate_model()
     model.plot_model_predicitons()
@@ -63,14 +57,11 @@ def start_model_estimation(arg_list):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Estimate dynamics model from flight log.')
-    parser.add_argument('--model', metavar='model', type=str,
-                        default='quadrotor_model',
-                        help='Parametric Model Type [quadrotor_model, quad_plane_model, delta_quad_plane_model, tilt_wing_model]')
     parser.add_argument('log_path', metavar='log_path', type=str,
                         help='The path of the log to process relative to the project directory.')
     parser.add_argument('--data_selection', metavar='data_selection', type=str2bool, default=False,
                         help='the path of the log to process relative to the project directory.')
-    parser.add_argument('--config', metavar='config', type=str, default='quadrotor',
+    parser.add_argument('--config', metavar='config', type=str, default='configs/quadrotor_model.yaml',
                         help='Configuration file path for pipeline configurations')
     arg_list = parser.parse_args()
-    start_model_estimation(arg_list)
+    start_model_estimation(**vars(arg_list))
