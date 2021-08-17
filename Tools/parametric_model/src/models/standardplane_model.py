7f81627a253ef4c3ece138cc32434254a78dbdd5
Jaeyoung-Lim <jalim@ethz.ch>
2021-08-17 13:46:45 +0200

    Replace AoA to angle_of_attack

--- Tools/parametric_model/src/models/standardplane_model.py
+++ Tools/parametric_model/src/models/standardplane_model.py
@@ -37,7 +37,7 @@ class StandardPlaneModel(DynamicsModel):
             # Aerodynamics features
             airspeed_mat = self.data_df[[
                 "V_air_body_x", "V_air_body_y", "V_air_body_z"]].to_numpy()
-            aoa_mat = self.data_df[["AoA"]].to_numpy()
+            aoa_mat = self.data_df[["angle_of_attack"]].to_numpy()
             aero_model = StandardWingModel(self.aerodynamics_dict)
             X_aero_forces, aero_coef_list = aero_model.compute_aero_force_features(airspeed_mat, aoa_mat)
 
@@ -88,7 +88,8 @@ class StandardPlaneModel(DynamicsModel):
             # Aerodynamics features
             airspeed_mat = self.data_df[[
                 "V_air_body_x", "V_air_body_y", "V_air_body_z"]].to_numpy()
-            aoa_mat = self.data_df[["AoA"]].to_numpy()
+            aoa_mat = self.data_df[["angle_of_attack"]].to_numpy()
+
             aero_model = StandardWingModel(self.aerodynamics_dict)
             X_aero_moments, aero_moments_coef_list = aero_model.compute_aero_moment_features(airspeed_mat, aoa_mat)
 
