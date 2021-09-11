b6d0ba527eb4c7525600e92e356219fae4affb92
Manumerous <manuel.galliker@gmx.ch>
2021-09-11 16:44:19 -0700

    Fix naming of prediciton error plots

--- Tools/parametric_model/src/models/dynamics_model.py
+++ Tools/parametric_model/src/models/dynamics_model.py
@@ -419,23 +419,23 @@ class DynamicsModel():
         if (self.estimate_forces and self.estimate_moments):
             y_forces_pred = y_pred[0:self.y_forces.shape[0]]
             y_moments_pred = y_pred[self.y_forces.shape[0]:]
-            model_plots.plot_accel_predeictions(
+            model_plots.plot_force_predeictions(
                 self.y_forces, y_forces_pred, self.data_df["timestamp"])
-            model_plots.plot_angular_accel_predeictions(
+            model_plots.plot_moment_predeictions(
                 self.y_moments, y_moments_pred, self.data_df["timestamp"])
             model_plots.plot_airspeed_and_AoA(
                 self.data_df[["V_air_body_x", "V_air_body_y", "V_air_body_z", "angle_of_attack"]], self.data_df["timestamp"])
 
         elif (self.estimate_forces):
             y_forces_pred = y_pred
-            model_plots.plot_accel_predeictions(
+            model_plots.plot_force_predeictions(
                 self.y_forces, y_forces_pred, self.data_df["timestamp"])
             model_plots.plot_airspeed_and_AoA(
                 self.data_df[["V_air_body_x", "V_air_body_y", "V_air_body_z", "angle_of_attack"]], self.data_df["timestamp"])
 
         elif (self.estimate_moments):
             y_moments_pred = y_pred
-            model_plots.plot_angular_accel_predeictions(
+            model_plots.plot_moment_predeictions(
                 self.y_moments, y_moments_pred, self.data_df["timestamp"])
             model_plots.plot_airspeed_and_AoA(
                 self.data_df[["V_air_body_x", "V_air_body_y", "V_air_body_z", "angle_of_attack"]], self.data_df["timestamp"])
