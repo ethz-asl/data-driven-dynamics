9401e8e9eaf98e4f3c9ba90ae420d44ddeb5d299
sjschlapbach <julius.schlapbach@gmx.net>
2022-11-20 16:07:13 +0100

    wip: add first implementation with separate model to the pipeline, which can be applied to simulation data

---
+++ Tools/parametric_model/src/models/aerodynamic_models/linear_wing_model.py
@@ -0,0 +1,219 @@
+"""
+ *
+ * Copyright (c) 2021 Manuel Yves Galliker
+ *               2021 Autonomous Systems Lab ETH Zurich
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name Data Driven Dynamics nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+"""
+
+__author__ = "Julius Schlapbach"
+__maintainer__ = "Julius Schlapbach"
+__license__ = "BSD 3"
+
+import math
+import numpy as np
+
+from src.tools.math_tools import cropped_sym_sigmoid
+from scipy.spatial.transform import Rotation
+from progress.bar import Bar
+
+"""
+The standard wing model is conform to PX4's standard plane and models
+ a wing with ailerons but without flaps. For reference see:
+ 
+https://docs.px4.io/master/en/airframes/airframe_reference.html#standard-plane
+ """
+
+
+class LinearWingModel():
+    def __init__(self, config_dict, mass):
+        self.stall_angle = config_dict["stall_angle_deg"]*math.pi/180.0
+        self.sig_scale_fac = config_dict["sig_scale_factor"]
+        self.air_density = 1.225
+        self.area = config_dict["area"]
+        self.mass = mass
+
+    def compute_wing_force_features(self, v_airspeed, angle_of_attack, elevator_input, flight_path_angle):
+        """
+        Model description:
+
+        Compute lift and drag forces in stability axis frame.
+
+        Lift force as a linear function of the angle of attack and the elevator input
+        F_Lift = 0.5 * density * area * V_air_xz^2 * (c_L_0 + c_L_alpha * alpha + c_L_delta_e * delta_e)
+
+        Drag force as a quadratic function of the lift coefficient
+        F_Drag = 0.5 * density * area * V_air_xz^2 * (c_D_0 + 1 / (pi * e * ar) * c_L^2)
+
+        Coefficients for optimization:
+        c_L_0, c_L_alpha, c_L_delta_e, c_D_0, 1/ar
+
+        :param v_airspeed: airspeed in m/s
+        :param angle_of_attack: angle of attack in rad
+
+        :return: regression matrix X for the estimation of x- and z-forces for a single feature
+        """
+
+        # compute dynamic pressure times wing area
+        dyn_pressure = 0.5 * self.air_density * self.area * \
+            (v_airspeed[0]**2 + v_airspeed[2]**2)
+        X_wing_aero_frame = np.zeros((3, 6))
+
+        # compute lift force
+        X_wing_aero_frame[2, 0] = - dyn_pressure
+        X_wing_aero_frame[2, 1] = - dyn_pressure * angle_of_attack
+        X_wing_aero_frame[2, 2] = - dyn_pressure * elevator_input
+
+        # lift offset is a function of the flight path angle
+        X_wing_aero_frame[2, 5] = - self.mass * 9.81 * math.sin(flight_path_angle)
+
+        # # Compute Drag force coeffiecients:
+        # X_wing_aero_frame[0, 0] = -flow_attached_region * q_x_A
+        # X_wing_aero_frame[0, 1] = - \
+        #     flow_attached_region * q_x_A * angle_of_attack
+        # X_wing_aero_frame[0, 2] = -flow_attached_region * \
+        #     q_x_A * angle_of_attack**2
+        # X_wing_aero_frame[0, 3] = -stall_region * \
+        #     (1 - math.sin(angle_of_attack)**2)*q_x_A
+        # X_wing_aero_frame[0, 4] = -stall_region * \
+        #     (math.sin(angle_of_attack)**2)*q_x_A
+        # # Compute Lift force coefficients:
+        # X_wing_aero_frame[2, 5] = -flow_attached_region*q_x_A
+        # X_wing_aero_frame[2, 6] = -flow_attached_region*q_x_A*angle_of_attack
+        # X_wing_aero_frame[2, 7] = -stall_region * \
+        #     q_x_A * math.sin(2*angle_of_attack)
+
+        # Transorm from stability axis frame to body FRD frame
+        R_aero_to_body = Rotation.from_rotvec(
+            [0, -angle_of_attack, 0]).as_matrix()
+        X_wing_body_frame = R_aero_to_body @ X_wing_aero_frame
+        X_wing_body_frame = X_wing_body_frame.flatten()
+        return X_wing_body_frame
+
+    # def compute_wing_moment_features(self, v_airspeed, angle_of_attack, angle_of_sideslip):
+    #     """
+    #     Model description:
+
+    #     TODO: Add description
+    #     """
+    #     q_xz = 0.5 * self.air_density * \
+    #         (v_airspeed[0]**2 + v_airspeed[2]**2)  # TODO Take dynamic pressure
+    #     q_xy = 0.5 * self.air_density * (v_airspeed[0]**2 + v_airspeed[1]**2)
+
+    #     X_wing_aero_frame = np.zeros((3, 3))
+
+    #     # region interpolation using a symmetric sigmoid function
+    #     # 0 in linear/quadratic region, 1 in post-stall region
+    #     stall_region = cropped_sym_sigmoid(
+    #         angle_of_attack, x_offset=self.stall_angle, scale_fac=self.sig_scale_fac)
+    #     # 1 in linear/quadratic region, 0 in post-stall region
+    #     flow_attached_region = 1 - stall_region
+
+    #     # Compute Roll Moment coeffiecients:
+
+    #     # Compute Pitch Moment coeffiecients:
+    #     X_wing_aero_frame[1, 0] = flow_attached_region * q_xz * self.area
+    #     X_wing_aero_frame[1, 1] = flow_attached_region * \
+    #         q_xz * self.area * angle_of_attack
+
+    #     # TODO: Compute Yaw Moment coeffiecients:
+    #     X_wing_aero_frame[2, 2] = flow_attached_region * \
+    #         q_xy * self.area * angle_of_sideslip
+
+    #     # Transorm from stability axis frame to body FRD frame
+    #     R_aero_to_body = Rotation.from_rotvec(
+    #         [0, -angle_of_attack, 0]).as_matrix()
+    #     X_wing_body_frame = R_aero_to_body @ X_wing_aero_frame
+    #     X_wing_body_frame = X_wing_body_frame.flatten()
+    #     return X_wing_body_frame
+
+    def compute_aero_force_features(self, v_airspeed_mat, angle_of_attack_vec, elevator_vec, gamma_vec):
+        """
+        Inputs:
+        :param v_airspeed_mat: airspeed in m/s with format numpy array of dimension (n,3) with columns for [v_a_x, v_a_y, v_a_z]
+        :param angle_of_attack_vec: angle of attack in rad with format vector of size (n) with corresponding AoA values
+
+        Returns:
+        :return: regression matrix X for the estimation of x- and z-forces
+        """
+        X_aero = self.compute_wing_force_features(
+            v_airspeed_mat[0, :], angle_of_attack_vec[0], elevator_vec[0], gamma_vec[0])
+        aero_features_bar = Bar(
+            'Feature Computatiuon', max=v_airspeed_mat.shape[0])
+        for i in range(1, len(angle_of_attack_vec)):
+            X_curr = self.compute_wing_force_features(
+                v_airspeed_mat[i, :], angle_of_attack_vec[i], elevator_vec[i], gamma_vec[i])
+            X_aero = np.vstack((X_aero, X_curr))
+            aero_features_bar.next()
+        aero_features_bar.finish()
+
+        coef_dict = {
+            'c_L_0': {"lin": {"x": 'c_L_0_x', "y": 'c_L_0_y', "z": 'c_L_0_z'}},
+            'c_L_alpha': {"lin": {"x": 'c_L_alpha_x', "y": 'c_L_alpha_y', "z": 'c_L_alpha_z'}},
+            'c_L_delta': {"lin": {"x": 'c_L_delta_x', "y": 'c_L_delta_y', "z": 'c_L_delta_z'}},
+            'c_D_0': {"lin": {"x": 'c_D_0_x', "y": 'c_D_0_y', "z": 'c_D_0_z'}},
+            'inv_ar': {"lin": {"x": 'inv_ar_x', "y": 'inv_ar_y', "z": 'inv_ar_z'}},
+            'const': {"lin": {"x": 'const_x', "y": 'const_y', "z": 'const_z'}},
+        }
+        col_names = ["c_L_0_x", "c_L_0_y", "c_L_0_z",
+                     "c_L_alpha_x", "c_L_alpha_y", "c_L_alpha_z",
+                     "c_L_delta_x", "c_L_delta_y", "c_L_delta_z",
+                     "c_D_0_x", "c_D_0_y", "c_D_0_z",
+                     "inv_ar_x", "inv_ar_y", "inv_ar_z",
+                     "const_x", "const_y", "const_z"]
+
+        return X_aero, coef_dict, col_names
+
+    # def compute_aero_moment_features(self, v_airspeed_mat, angle_of_attack_vec, angle_of_sideslip_vec):
+    #     """
+    #     Inputs:
+
+    #     v_airspeed_mat: numpy array of dimension (n,3) with columns for [v_a_x, v_a_y, v_a_z]
+    #     angle_of_attack_vec: vector of size (n) with corresponding AoA values
+    #     """
+    #     print("Starting computation of aero moment features...")
+    #     X_aero = self.compute_wing_moment_features(
+    #         v_airspeed_mat[0, :], angle_of_attack_vec[0], angle_of_sideslip_vec[0])
+    #     aero_features_bar = Bar(
+    #         'Feature Computatiuon', max=v_airspeed_mat.shape[0])
+    #     for i in range(1, len(angle_of_attack_vec)):
+    #         X_curr = self.compute_wing_moment_features(
+    #             v_airspeed_mat[i, :], angle_of_attack_vec[i], angle_of_sideslip_vec[i])
+    #         X_aero = np.vstack((X_aero, X_curr))
+    #         aero_features_bar.next()
+    #     aero_features_bar.finish()
+    #     coef_dict = {
+    #         "c_m_x_wing_xz_offset": {"rot":{ "x": "c_m_x_wing_xz_offset_x","y": "c_m_x_wing_xz_offset_y","z":"c_m_x_wing_xz_offset_z"}},
+    #         "c_m_x_wing_xz_lin": {"rot":{ "x": "c_m_x_wing_xz_lin_x","y": "c_m_x_wing_xz_lin_y","z":"c_m_x_wing_xz_lin_z"}},
+    #         "c_m_z_wing_lin": {"rot":{ "x": "c_m_z_wing_lin_x","y": "c_m_z_wing_lin_y","z":"c_m_z_wing_lin_z"}},
+    #     }
+    #     col_names = ["c_m_x_wing_xz_offset_x", "c_m_x_wing_xz_offset_y", "c_m_x_wing_xz_offset_z",
+    #                 "c_m_x_wing_xz_lin_x", "c_m_x_wing_xz_lin_y", "c_m_x_wing_xz_lin_z",
+    #                 "c_m_z_wing_lin_x", "c_m_z_wing_lin_y", "c_m_z_wing_lin_z"]
+    #     return X_aero, coef_dict, col_names
---
+++ Tools/parametric_model/src/models/simple_fixedwing_model.py
@@ -0,0 +1,92 @@
+__author__ = "Manuel Galliker"
+__maintainer__ = "Manuel Galliker"
+__license__ = "BSD 3"
+
+"""Model to estimate the system parameters of gazebos standart vtol quadplane:
+https://docs.px4.io/master/en/simulation/gazebo_vehicles.html#standard_vtol """
+
+
+import numpy as np
+import math
+
+from .dynamics_model import DynamicsModel
+from .rotor_models import RotorModel
+from sklearn.linear_model import LinearRegression
+from .model_config import ModelConfig
+from .aerodynamic_models import LinearWingModel, ControlSurfaceModel
+
+
+"""This model estimates forces and moments for quad plane as for example the standard vtol in gazebo."""
+
+
+class FixedWingPlaneModel(DynamicsModel):
+    def __init__(self, config_file, model_name="simple_fixedwing_model"):
+        self.config = ModelConfig(config_file)
+        super(FixedWingPlaneModel, self).__init__(
+            config_dict=self.config.dynamics_model_config)
+        self.mass = self.config.model_config["mass"]
+        self.moment_of_inertia = np.diag([self.config.model_config["moment_of_inertia"]["Ixx"],
+                                         self.config.model_config["moment_of_inertia"]["Iyy"], self.config.model_config["moment_of_inertia"]["Izz"]])
+
+        self.model_name = model_name
+
+        self.rotor_config_dict = self.config.model_config["actuators"]["rotors"]
+        self.aero_config_dict = self.config.model_config["actuators"]["control_surfaces"]
+        self.aerodynamics_dict = self.config.model_config["aerodynamics"]
+
+    def prepare_force_regression_matrices(self):
+        # Accelerations
+        accel_mat = self.data_df[[
+            "acc_b_x", "acc_b_y", "acc_b_z"]].to_numpy()
+        force_mat = accel_mat * self.mass
+        self.y_forces = (force_mat).flatten()
+        self.data_df[["measured_force_x", "measured_force_y",
+                     "measured_force_z"]] = force_mat
+
+        # Aerodynamics features
+        airspeed_mat = self.data_df[[
+            "V_air_body_x", "V_air_body_y", "V_air_body_z"]].to_numpy()
+        aoa_mat = self.data_df[["angle_of_attack"]].to_numpy()
+        elevator_vec = self.data_df["u7"].to_numpy()
+        gamma_vec = - np.arctan2(self.data_df['vz'], self.data_df['vx'])
+        aero_model = LinearWingModel(self.aerodynamics_dict, self.mass)
+        X_aero, coef_dict_aero, col_names_aero = aero_model.compute_aero_force_features(
+            airspeed_mat, aoa_mat, elevator_vec, gamma_vec)
+        self.data_df[col_names_aero] = X_aero
+        self.coef_dict.update(coef_dict_aero)
+        self.y_dict.update({"lin":{"x":"measured_force_x","y":"measured_force_y","z":"measured_force_z"}})
+
+    def prepare_moment_regression_matrices(self):
+        # Angular acceleration
+        moment_mat = np.matmul(self.data_df[[
+            "ang_acc_b_x", "ang_acc_b_y", "ang_acc_b_z"]].to_numpy(), self.moment_of_inertia)
+        self.y_moments = moment_mat.flatten()
+        self.data_df[["measured_moment_x", "measured_moment_y",
+                     "measured_moment_z"]] = moment_mat
+
+        # Aerodynamics features
+        airspeed_mat = self.data_df[[
+            "V_air_body_x", "V_air_body_y", "V_air_body_z"]].to_numpy()
+        aoa_mat = self.data_df[["angle_of_attack"]].to_numpy()
+        sideslip_mat = self.data_df[["angle_of_sideslip"]].to_numpy()
+
+        aero_model = LinearWingModel(self.aerodynamics_dict, self.mass)
+        X_aero, coef_dict_aero, col_names_aero = aero_model.compute_aero_moment_features(
+            airspeed_mat, aoa_mat, sideslip_mat)
+        self.data_df[col_names_aero] = X_aero
+        self.coef_dict.update(coef_dict_aero)
+
+        aero_config_dict = self.aero_config_dict
+        for aero_group in aero_config_dict.keys():
+            aero_group_list = self.aero_config_dict[aero_group]
+
+            for config_dict in aero_group_list:
+                controlsurface_input_name = config_dict["dataframe_name"]
+                u_vec = self.data_df[controlsurface_input_name].to_numpy()
+                control_surface_model = ControlSurfaceModel(config_dict, self.aerodynamics_dict, u_vec)
+                X_controls, coef_dict_controls, col_names_controls = control_surface_model.compute_actuator_moment_matrix(
+                    airspeed_mat, aoa_mat)
+                self.data_df[col_names_controls] = X_controls
+                self.coef_dict.update(coef_dict_controls)
+
+        self.y_dict.update({"rot":{"x":"measured_moment_x","y":"measured_moment_y","z":"measured_moment_z"}})
