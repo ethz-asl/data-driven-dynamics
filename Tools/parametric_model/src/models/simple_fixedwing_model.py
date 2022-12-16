7ab73ef1b0246ee834a3c072f13d9b61b314ae69
sjschlapbach <julius.schlapbach@gmx.net>
2022-12-16 14:03:33 +0100

    wip: add torque and thrust setpoints as model parameters

--- Tools/parametric_model/src/models/simple_fixedwing_model.py
+++ Tools/parametric_model/src/models/simple_fixedwing_model.py
@@ -71,7 +71,7 @@ class SimpleFixedWingModel(DynamicsModel):
         airspeed_mat = self.data_df[[
             "V_air_body_x", "V_air_body_y", "V_air_body_z"]].to_numpy()
         aoa_mat = self.data_df[["angle_of_attack"]].to_numpy()
-        elevator_inputs = self.data_df["u7"].to_numpy()
+        elevator_inputs = self.data_df["elevator"].to_numpy()
 
         aero_model = LinearWingModel(self.aerodynamics_dict)
         X_aero, coef_dict_aero, col_names_aero = aero_model.compute_aero_force_features(
@@ -96,7 +96,7 @@ class SimpleFixedWingModel(DynamicsModel):
         sideslip_mat = self.data_df[["angle_of_sideslip"]].to_numpy()
         angular_vel_mat = self.data_df[[
             "ang_vel_x", "ang_vel_y", "ang_vel_z"]].to_numpy()
-        elevator_inputs = self.data_df["u7"].to_numpy()
+        elevator_inputs = self.data_df["elevator"].to_numpy()
 
         aero_model = LinearWingModel(self.aerodynamics_dict)
         X_aero, coef_dict_aero, col_names_aero = aero_model.compute_aero_moment_features(
